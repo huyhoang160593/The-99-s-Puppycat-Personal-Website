fc8fad5e443043a28e45187a1d64db95c06b1db7
The 99's Puppycat <huyhoang160593@yahoo.com>
2024-05-04 14:38:24 +0700

    add import map and using module script instead of typical script

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+import { default as Alpine } from "alpinejs";
+
+Alpine.store('themeSetting', {
+	theme: 'light',
+
+	toggleTheme() {
+		switch (this.theme) {
+			case 'light': this.theme = 'dark'; break;
+			case 'dark': this.theme = 'light'; break;
+		}
+	}
+})
+Alpine.start()
