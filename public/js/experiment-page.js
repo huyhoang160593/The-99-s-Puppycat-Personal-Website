c483fb28ef67a2db770ffc15b25e03925e90a132
The 99's Puppycat <huyhoang160593@yahoo.com>
2024-06-16 13:30:12 +0700

    testing

--- public/js/experiment-page.js
+++ public/js/experiment-page.js
@@ -14,12 +14,45 @@ Alpine.store("global", {
 			const eyeX = eyeElement.offsetLeft + (eyeElement.clientWidth / 2)
 			const eyeY = eyeElement.offsetTop + (eyeElement.clientHeight / 2)
 			const rotationRadian = Math.atan2(e.pageX - eyeX, e.pageY - eyeY)
-			const rotationDegree = (rotationRadian * (180/ Math.PI) * (-1)) + 180
+			const rotationDegree = (rotationRadian * (180 / Math.PI) * (-1)) + 180
 			/** {@link https://animejs.com/documentation/ | Animejs documentation} */
 			anime({
 				targets: eyeElement,
 				rotate: rotationDegree
 			})
+		}
+	},
+
+	/**
+	 * @param {MouseEvent & { target: HTMLButtonElement }} e
+	 */
+	onMouseEnterHandle(e) {
+		anime({
+			targets: e.target,
+			translateX: anime.random(-e.target.offsetLeft, document.body.clientWidth - e.target.offsetLeft),
+			translateY: anime.random(-e.target.offsetTop, document.body.clientHeight - e.target.offsetTop)
+		})
+		e.target.blur()
+	},
+
+	/**
+	 * @param {MouseEvent & { target: HTMLButtonElement }} e
+	 */
+	onForgiveHandle(e) {
+		const fireworkElement = (document.getElementById("firework-group"))
+		const forgiveElement = document.getElementById("forgive-success")
+		const iframeCollection = document.getElementsByTagName('iframe')
+
+		if (fireworkElement.classList.contains('hidden')) {
+			fireworkElement.classList.remove('hidden')
+		}
+		if (forgiveElement.classList.contains('hidden')) {
+			forgiveElement.classList.remove('hidden')
+		}
+		if (iframeCollection.length > 0) {
+			if (iframeCollection.item(0).classList.contains('hidden')) {
+				iframeCollection.item(0).classList.remove('hidden')
+			}
 		}
 	}
 })
